25f6e83610b9d2d67ba1eb77df96f3e35bde034e
011000101101 <011000101101@gmx.de>
2019-08-10 14:34:28 +0200

    revised recurrent layer structure and restructured training data into batches of equal numbers of time steps per dataset

--- NeuronalNetworks/LSTM_prototype/main.py
+++ NeuronalNetworks/LSTM_prototype/main.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 # read training and test data from file and format it...
-input_file = open("training_data.txt", "r")
+print("started")
+input_file = open("test.txt", "r")
 lines = input_file.read().splitlines()
 input_coord_lists = []
 output_costs = []
@@ -39,29 +40,88 @@ print(input_coord_lists)
 print("target array:")
 print(output_costs)
 
-# Data = [[[(i+j)/100] for i in range(5)] for j in range(100)]
-# target = [((i+5)/100) for i in range(100)]
-# data = np.array(Data, dtype= float)
-# target= np.array(target, dtype= float)
+list_of_batches = []
+list_of_batch_target_lists = []
+list_of_batch_normalization_factor_lists = []
+list_of_batches_validation = []
+list_of_batch_target_lists_validation = []
+list_of_batch_normalization_factor_lists_validation = []
 
-x_train, x_test, y_train, y_test = train_test_split(input_coord_lists, output_costs, test_size=0.2, random_state=4)
+for index in range(len(input_coord_lists)):
+    input_data_set = input_coord_lists[index]
+    while len(list_of_batches) < len(input_data_set) - 1:
+        list_of_batches.append([])
+        list_of_batch_target_lists.append([])
+        list_of_batch_normalization_factor_lists.append([])
 
-# print(x_train)
+        list_of_batches_validation.append([])
+        list_of_batch_target_lists_validation.append([])
+        list_of_batch_normalization_factor_lists_validation.append([])
+
+    list_of_batches[len(input_data_set) - 2].append(input_data_set)
+    list_of_batch_target_lists[len(input_data_set) - 2].append(output_costs[index])
+    list_of_batch_normalization_factor_lists[len(input_data_set) - 2].append(normalization_factors[index])
+    
+print("data sorted by length:")
+print("input data:")
+print(list_of_batches)
+print("output data data:")
+print(list_of_batch_target_lists)
+print("normalization factors:")
+print(list_of_batch_normalization_factor_lists)
+
+print("length of batches:")
+for batch in list_of_batches:
+    print(len(batch))
+
+batch_size = 1000
+
+for batch_index in range(len(list_of_batches)):
+    while len(list_of_batches[batch_index]) % batch_size != 0:
+        list_of_batches[batch_index].pop()
+        list_of_batch_target_lists[batch_index].pop()
+        list_of_batch_normalization_factor_lists[batch_index].pop()
+    print(len(list_of_batches[batch_index]))
+
+for batch_index in range(len(list_of_batches)):
+    for counter in range(batch_size):
+        list_of_batches_validation[batch_index].append(list_of_batches[batch_index].pop())
+        list_of_batch_target_lists_validation[batch_index].append(list_of_batch_target_lists[batch_index].pop())
+        list_of_batch_normalization_factor_lists_validation[batch_index].append(list_of_batch_normalization_factor_lists[batch_index].pop())
+
 
 model = tf.keras.models.Sequential()
 
-model.add(tf.keras.layers.LSTM(1, batch_input_shape=(None, None, 2), return_sequences=False))
+model.add(tf.keras.layers.LSTM(10, batch_input_shape=(batch_size, None, 2), return_sequences=False, stateful=True))
+model.add(tf.keras.layers.Dense(1))
 
 model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-history = model.fit(x_train, y_train, epochs=800, validation_data=(x_test, y_test))
+for index in range(len(list_of_batches)):
 
-results = model.predict(x_test)
+    # x_train, x_test, y_train, y_test = train_test_split(list_of_batches[index], list_of_batch_target_lists[index], test_size=0.2, random_state=4)
 
-plt.scatter(range(20), results, c='r')
-plt.scatter(range(20), y_test, c='g')
-plt.show()
+    # x_train = np.asarray(x_train).reshape(len(x_train), 2, 2)
+    x_train = np.asarray(list_of_batches[index]).reshape(len(list_of_batches[index]), index + 2, 2)
+    y_train = np.asarray(list_of_batch_target_lists[index])
+    print(x_train)
+    print(len(x_train))
+    x_test = np.asarray(list_of_batches_validation[index]).reshape(len(list_of_batches_validation[index]), index + 2, 2)
+    y_test = np.asarray(list_of_batch_target_lists_validation[index])
+# print(x_train)
 
-plt.plot(history.history['loss'])
-plt.show()
+    history = model.fit(x_train, y_train, epochs=100, validation_data=(x_test, y_test))
+
+    results = model.predict(x_test)
+    print(results.shape)
+    print(y_test.shape)
+    print(y_test.reshape(len(y_test), 1).shape)
+    plt.scatter(range(20), results[:, 0], c='r')
+    plt.scatter(range(20), y_test, c='g')
+    plt.show()
+
+    plt.plot(history.history['loss'])
+    plt.show()
+
+    model.reset_states()
