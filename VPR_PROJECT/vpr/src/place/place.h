91179979953e34740c9d71f81bc59ad3215de9ca
011000101101 <011000101101@gmx.de>
2020-09-07 02:06:36 +0200

    changed mode to legacy and successfully tested all 4 modes again

--- VPR_PROJECT/vpr/src/place/place.h
+++ VPR_PROJECT/vpr/src/place/place.h
@@ -9,7 +9,7 @@
  * 3: is ml_integration LSTM = wiring cost estimation via LSTM NN, includes features of reference
  * 4: is ml_integration CNN = wiring cost estimation via CNN NN, includes features of reference
  */
-#define MODE 3
+#define MODE 0
 /// enables debugging of NN integration and training data generation
 #define DEBUG_NN_INTEGRATION 0
 
